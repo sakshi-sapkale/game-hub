9d43948ff6cdd739c75b639e60cf27563386f662
Sakshi Sapakle <sakshisapkale77@gmail.com>
2024-03-22 13:18:28 +0530

    build a navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,10 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+export const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px" />
+      <Text>Nav</Text>
+    </HStack>
+  );
+};
